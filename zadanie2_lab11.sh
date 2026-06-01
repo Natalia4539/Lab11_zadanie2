356db7cb366e3feaba29ec940f7e93503b1235f5
Natalia <289887@student.pwr.edu.pl>
2026-06-01 15:45:10 +0200

    Update zadanie2_lab11.sh

--- zadanie2_lab11.sh
+++ zadanie2_lab11.sh
@@ -4,8 +4,8 @@ read -p "Podaj plik do podpisania" plik
 plik_klucz="${plik}.klucz"
 
 if [ ! -f "$plik_klucz" ]; then
-    echo "[-] Błąd: Brak pliku z kluczem ($plik_klucz). Najpierw zaszyfruj plik!"
-    exit 1
+    echo "Błąd: Brak pliku z kluczem "
+    exit -1
 fi
 
 openssl dgst -sha256 -hmac "$(cat "$plik_klucz")" -out "${plik}.sig" "$plik"
